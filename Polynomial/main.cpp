bb538fc9f288ade45c2de4d9b610ad539b7c65a2
Patrick A Kelly <p1991atrick@gmail.com>
2016-03-29 19:15:11 -0700

    Initial Commit

---
+++ Polynomial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Polynomial
+//
+//  Created by Gordon Freeman on 3/29/16.
+//  Copyright © 2016 Patrick Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
